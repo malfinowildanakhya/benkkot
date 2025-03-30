9bbaa8c6ad50f75d9c2fc3ac0bff8746eca7577d
malfinowildanakhya <johndoe@example.com1112022468@mhs.dinus.ac.id>
2025-03-30 17:03:58 +0700

    Menambahkan perubahan pada seeder, view, dan route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,52 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Models\Periksa;
+use App\Models\DetailPeriksa;
+use App\Models\Obat;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/dokter', function () {
+    $periksas = Periksa::all(); 
+    $obats = Obat::all(); 
+    return view('dokter.dashboard', compact('periksas', 'obats'));
+});
+
+Route::get('/dokter/obat', function () {
+    $obats = Obat::all(); 
+    return view('dokter.obat', compact('obats'));
+});
+
+Route::get('/dokter/periksa', function () {
+    $periksas = Periksa::all(); 
+    $obats = Obat::all();
+    return view('dokter.periksa', compact('periksas', 'obats'));
+});
+
+
+
+Route::get('/pasien', function () {
+    $periksas = Periksa::all(); 
+    return view('pasien.dashboard', compact('periksas'));
+});
+Route::get('/pasien/periksa', function () {
+    $periksas = Periksa::all(); 
+    return view('pasien.periksa', compact('periksas'));
+});
+
+Route::get('/pasien/riwayat', function () {
+    $Detailperiksas = DetailPeriksa::all(); 
+    $periksas = Periksa::all();
+    return view('pasien.riwayat', compact('Detailperiksas', 'periksas'));
 });
